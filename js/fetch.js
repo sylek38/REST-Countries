03daa536dd94e1a06c248ea0233894e370a9d1d8
Sylek <sylek38@gmail.com>
2021-09-08 16:52:19 +0200

    add sorting and filtering data

--- js/fetch.js
+++ js/fetch.js
@@ -1,26 +1,27 @@
 //fetch api
 const allCountries = "https://restcountries.eu/rest/v2/all";
-
+let countries;
 fetch(allCountries)
     .then(res => res.json())
-    .then(data => displayTableValues(data))
+    .then(data => {
+        countries = data;
+        displayTable(countries)
+    })
 
-const rows = document.querySelector('tbody').children;
+function displayTable(countries) {
 
-function displayTableValues(countries) {
+    // console.log(countries)
 
-    console.log(countries)
-    
     tableRows = countries.map((country, idx) => {
-        
+
             return `
         <tr>
-            <td>${displayProps(country.name)}</td>
-            <td>${displayProps(country.currencies)}</td>
-            <td>${displayProps(country.languages)}</td>
-            <td>${displayProps(country.population)}</td>
-            <td>${displayProps(country.area)}</td>
-            <td><img class="flag-container" src="${displayProps(country.flag)}" alt="the flag of ${displayProps(country.name)}"/></td>
+            <td>${displayValues(country.name)}</td>
+            <td>${displayValues(country.currencies)}</td>
+            <td>${displayValues(country.languages)}</td>
+            <td>${displayValues(country.population)}</td>
+            <td>${displayValues(country.area)}</td>
+            <td><img class="flag-container" src="${displayValues(country.flag)}" alt="the flag of ${displayValues(country.name)}"/></td>
         </tr>`;
         })
         .join("");
@@ -29,20 +30,14 @@ function displayTableValues(countries) {
 
 }
 
-// TODO: refactor this function. it can be done better
-displayProps = (countryProp) => {
-    // console.log(typeof(countryProp), countryProp)
+displayValues = (countryProp) => {
     let tabl = [];
 
     if (countryProp === null) {
         tabl.push("---");
-    }
-
-    else if (countryProp === "number") {
-        tabl.push(parseInt(countryProp.toLocaleString()))
-    }
-
-    else if (typeof (countryProp) === "object") {
+    } else if (typeof (countryProp) === "number") {
+        tabl.push(countryProp.toLocaleString())
+    } else if (typeof (countryProp) === "object") {
         for (let prop in countryProp) {
             for (let nestedProp in countryProp[prop]) {
                 if (nestedProp === "name") {
@@ -58,103 +53,107 @@ displayProps = (countryProp) => {
     return tabl.join(",<br>");
 }
 
+// TODO:
+// getMaxPopulationValue = () => {
+
+// };
+
+const rows = document.querySelector('tbody').children;
+
 // FILTER BY KEYWORD
+function filterTable(keyword, min, max) {
 
-const keyword = document.querySelector("#keyword-inp").value.trim();
+    min = min ? parseInt(min) : 0;
+    max = max ? parseInt(max) : 1000000000;
 
- document.querySelector("#cta").addEventListener("click", e => {
-        e.preventDefault();
-        filterByKeyword(keyword)
+    console.log(min, max)
+
+    //filter by keyword and min/max
+    if (keyword !== "") {
+
+        console.log("keyword !== ''")
+
+        Array.from(rows).forEach(row => {
+            if (row.textContent.toLowerCase().includes(keyword.toLowerCase()) &&
+                min <= parseInt(row.children[3].textContent.replace(/\D/g, '')) &&
+                max >= parseInt(row.children[3].textContent.replace(/\D/g, ''))) {
+
+                row.style.display = "table-row"
+
+            } else {
+
+                row.style.display = "none";
+
+            }
+        });
+
+        // filter by min/max
+    } else {
+
+        console.log("keyword === ''")
+
+        Array.from(rows).forEach(row => {
+            if (min <= parseInt(row.children[3].textContent.replace(/\D/g, '')) &&
+                max >= parseInt(row.children[3].textContent.replace(/\D/g, ''))) {
+
+                row.style.display = "";
+
+            } else {
+
+                row.style.display = "none";
+
+            }
+        });
+    }
+
+}
+
+document.querySelector("#cta").addEventListener("click", e => {
+    e.preventDefault();
+    const min = document.querySelector("#min-population-inp").value;
+    const max = document.querySelector("#max-population-inp").value;
+    const keyword = document.querySelector("#keyword-inp").value.trim();
+
+    filterTable(keyword, min, max)
 });
 
 
 
-const filterByKeyword = keyword => {
-    // if (keyword ){}
+// SORT TABLE:
+function sortTableByColumn(table, column, asc = true) {
+    const directionModifier = asc ? 1 : -1;
+    const tBody = document.querySelector("#data");
 
-    // displayTableValues()
+    // Sort each row
+    const sortedRows = Array.from(rows).sort((a, b) => {
+        const aColumnValue = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        const bColumnValue = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+
+        return aColumnValue > bColumnValue ? (1 * directionModifier) : (-1 * directionModifier)
+
+    });
+
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th.sortable").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th.sortable:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th.sortable:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
+
 }
 
+document.querySelectorAll(".table th.sortable").forEach(th => {
+    th.addEventListener("click", () => {
+        const tableElement = th.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(th.parentElement.children, th);
+        const currentIsAsc = th.classList.contains("th-sort-asc");
 
-// set max inp value dynamically + set min inp value
-// const populationInputs = document.querySelectorAll(".num-inp");
-
-// const maxPopulationArr = [];
-// Array.from(rows).forEach(row => maxPopulationArr.push(row[3]));
-
-
-// populationInputs.forEach(input => {
-//     input.setAttribute("min", 0);
-//     input.setAttribute("max", Math.max(...maxPopulationArr));
-// })
-
-
-
-// show results by keyword
-
-// const min = document.querySelector("#min-population-inp").value;
-// const max = document.querySelector("#max-population-inp").value;
-// const keyword = document.querySelector("#keyword-inp").value.trim();
-
-// document.querySelector("#cta").addEventListener("click", e => {
-//     e.preventDefault();
-//     getKeyword(rows);
-
-
-
-    // Array.from(rows).forEach(row => {
-    //     console.log(row);
-    //     row.textContent.toLowerCase().includes(keyword.toLowerCase()) ?
-    //         row.style.display = "table-row"
-    //         :
-    //         row.style.display = "none";
-    // })
-
-    // console.log(min)
-    Array.from(rows).forEach((row, idx) => {
-        let populationTd = parseInt(row.children[3].innerText.replace(/\s/g, ""))
-        populationTd >= min && populationTd <= max ?
-            row.style.display = "table-row" 
-            :
-            row.style.display = "none"
-
+        sortTableByColumn(tableElement, headerIndex, !currentIsAsc);
     })
-
-
-// const getKeyword = rows => {
-//     [...rows].forEach(row => {
-//         console.log(row);
-//         row.textContent.toLowerCase().includes(keyword.toLowerCase()) ?
-//             row.style.display = "table-row" :
-//             row.style.display = "none";
-//     })
-// }
-
-// const getMinGetMax = rows => {
-//     [...rows].forEach((row, idx) => {
-//         let populationTd = parseInt(row.children[3].innerText.replace(/\s/g, ""))
-//         populationTd >= min && populationTd <= max ?
-//             row.style.display = "table-row" :
-//             row.style.display = "none"
-
-//     })
-// }
-
-
-
-//     // show results by keyword && values
-//     const min = parseInt(document.querySelector("#min-population-inp")).value;
-//     const max = parseInt(document.querySelector("#max-population-inp")).value;
-
-//     let columnList = [];
-
-//     for (row of rows) {
-//         let columnEl = row.children[3];
-//         if ((parseInt(columnEl.textContent) >= min) && (parseInt(columnEl.textContent) <= max) && (row.textContent.toLowerCase().includes(keyword.toLowerCase()))) {
-//             columnList.push(columnEl.textContent);
-//         }
-//     }
-//     document.querySelector("#data").innerHTML = columnList;
-
-
-// })
+})
