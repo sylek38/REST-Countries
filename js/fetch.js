6bc8180a4376f151584bb65774c70c723c29ac37
Sylek <sylek38@gmail.com>
2021-09-08 19:39:52 +0200

    add checking if column if numeric

--- js/fetch.js
+++ js/fetch.js
@@ -120,28 +120,35 @@ document.querySelector("#cta").addEventListener("click", e => {
 
 
 // SORT TABLE:
-function sortTableByColumn(table, column, asc = true) {
+function sortTableByColumn(table, column, asc = true, isNumeric) {
     const directionModifier = asc ? 1 : -1;
     const tBody = document.querySelector("#data");
 
-    // Sort each row
+    // TODO: the Area column still doesn't work. "---" and float case (0,44, 346,36 or 2,02 for example)
     const sortedRows = Array.from(rows).sort((a, b) => {
-        const aColumnValue = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
-        const bColumnValue = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+        if (isNumeric) {
+            console.log(parseInt(a.querySelector(`td:nth-child(${column + 1})`).textContent.replace(/\D/g, '')))
+            const aColumnValue = parseInt(a.querySelector(`td:nth-child(${column + 1})`).textContent.replace(/\D/g, ''));
+            const bColumnValue = parseInt(b.querySelector(`td:nth-child(${column + 1})`).textContent.replace(/\D/g, ''));
 
-        return aColumnValue > bColumnValue ? (1 * directionModifier) : (-1 * directionModifier)
+            return aColumnValue > bColumnValue ? (1 * directionModifier) : (-1 * directionModifier)
+            
 
+        } else {
+
+            const aColumnValue = a.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+            const bColumnValue = b.querySelector(`td:nth-child(${column + 1})`).textContent.trim();
+
+            return aColumnValue > bColumnValue ? (1 * directionModifier) : (-1 * directionModifier)
+        }
     });
 
-    // Remove all existing TRs from the table
     while (tBody.firstChild) {
         tBody.removeChild(tBody.firstChild);
     }
 
-    // Re-add the newly sorted rows
     tBody.append(...sortedRows);
 
-    // Remember how the column is currently sorted
     table.querySelectorAll("th.sortable").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
     table.querySelector(`th.sortable:nth-child(${column + 1})`).classList.toggle("th-sort-asc", asc);
     table.querySelector(`th.sortable:nth-child(${column + 1})`).classList.toggle("th-sort-desc", !asc);
@@ -153,7 +160,8 @@ document.querySelectorAll(".table th.sortable").forEach(th => {
         const tableElement = th.parentElement.parentElement.parentElement;
         const headerIndex = Array.prototype.indexOf.call(th.parentElement.children, th);
         const currentIsAsc = th.classList.contains("th-sort-asc");
+        const currentIsNumeric = th.classList.contains("numeric");
 
-        sortTableByColumn(tableElement, headerIndex, !currentIsAsc);
+        sortTableByColumn(tableElement, headerIndex, !currentIsAsc, currentIsNumeric);
     })
 })
